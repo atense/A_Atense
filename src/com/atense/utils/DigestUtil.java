ed7f01c142bfa42198cf786b331b49bf308e34ba
Kyle <lk_likai@126.com>
2015-11-04 18:01:07 +0800

    edit

--- src/com/atense/utils/DigestUtil.java
+++ src/com/atense/utils/DigestUtil.java
@@ -17,7 +17,7 @@ public class DigestUtil {
 	/**
      * Used to build output as Hex
      */
-    private static final char[] DIGITS_LOWER = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D',
+    private static final char[] DIGITS_UPPER = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D',
             'E', 'F'};
 
     /**
@@ -103,8 +103,8 @@ public class DigestUtil {
         final char[] out = new char[l << 1];
         // two characters form the hex value.
         for (int i = 0, j = 0; i < l; i++) {
-            out[j++] = DIGITS_LOWER[(0xF0 & data[i]) >>> 4];
-            out[j++] = DIGITS_LOWER[0x0F & data[i]];
+            out[j++] = DIGITS_UPPER[(0xF0 & data[i]) >>> 4];
+            out[j++] = DIGITS_UPPER[0x0F & data[i]];
         }
         return new String(out);
     }
